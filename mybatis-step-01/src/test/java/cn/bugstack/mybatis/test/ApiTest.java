b4bcf155c75b427766f540a2c4d84d1846320a8e
gumo <hu.yong@intellif.com>
2024-05-07 16:44:24 +0800

    feat: 一级缓存

--- mybatis-step-01/src/test/java/cn/bugstack/mybatis/test/ApiTest.java
+++ mybatis-step-01/src/test/java/cn/bugstack/mybatis/test/ApiTest.java
@@ -1,31 +1,21 @@
 package cn.bugstack.mybatis.test;
 
-import cn.bugstack.mybatis.builder.xml.XMLConfigBuilder;
-import cn.bugstack.mybatis.executor.Executor;
 import cn.bugstack.mybatis.io.Resources;
-import cn.bugstack.mybatis.mapping.Environment;
 import cn.bugstack.mybatis.session.*;
-import cn.bugstack.mybatis.session.defaults.DefaultSqlSession;
 import cn.bugstack.mybatis.test.dao.IActivityDao;
 import cn.bugstack.mybatis.test.po.Activity;
-import cn.bugstack.mybatis.transaction.Transaction;
-import cn.bugstack.mybatis.transaction.TransactionFactory;
 import com.alibaba.fastjson.JSON;
-import ognl.Ognl;
-import ognl.OgnlContext;
-import ognl.OgnlException;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.Reader;
-import java.util.List;
 
 /**
  * @author 小傅哥，微信：fustack
  * @description 单元测试
+ *  * https://louluan.blog.csdn.net/article/details/41280959
+ *  * https://dude6.com/article/168362.html
  * @date 2022/3/26
  * @github https://github.com/fuzhengwei
  * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
@@ -46,8 +36,14 @@ public class ApiTest {
         // 3. 测试验证
         Activity req = new Activity();
         req.setActivityId(100001L);
-        Activity res = dao.queryActivityById(req);
-        logger.info("测试结果：{}", JSON.toJSONString(res));
+
+        logger.info("测试结果：{}", JSON.toJSONString(dao.queryActivityById(req)));
+
+//         sqlSession.commit();
+        // sqlSession.clearCache();
+//         sqlSession.close();
+
+        logger.info("测试结果：{}", JSON.toJSONString(dao.queryActivityById(req)));
     }
 
 }
